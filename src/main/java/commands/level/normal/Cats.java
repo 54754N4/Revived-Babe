7712abedc87ebca1707b5b7a2fa419d00b767bc5
Mazen Anastas <lorenzo.mallerco@gmail.com>
2020-11-16 20:32:02 +0700

    Add: Cat facts command + json POJO for result conversion

---
+++ src/main/java/commands/level/normal/Cats.java
@@ -0,0 +1,49 @@
+package commands.level.normal;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Random;
+
+import com.google.gson.Gson;
+
+import bot.model.UserBot;
+import commands.model.DiscordCommand;
+import json.cat.facts.Results;
+import lib.HTTP;
+import net.dv8tion.jda.api.entities.Message;
+
+public class Cats extends DiscordCommand {
+	public static final String CAT_FACTS_API = "https://cat-fact.herokuapp.com/facts";
+	private static Random rand = new Random();
+	private static Gson gson = new Gson();
+	private static Results results;		// lazy loading for attributes that can throw
+
+	public Cats(UserBot bot, Message message) {
+		super(bot, message, "cats");
+	}
+
+	@Override
+	protected String helpMessage() {
+		return helpBuilder("", 
+			"-f or --facts\tretrieves random cat fact",
+			"For all cat lovers. Rawr~ <3");
+	}
+
+	@Override
+	protected void execute(String input) throws Exception {
+		Results results = getFacts();
+		int total = results.all.length;
+		println(results.all[rand.nextInt(total)].text);
+	}
+
+	public static Results getFacts() throws MalformedURLException, IOException {
+		if (results != null)
+			return results;
+		try (	// try-with-resources to auto-close close-able resources
+			HTTP.RequestBuilder builder = new HTTP.RequestBuilder(CAT_FACTS_API);
+			HTTP.ResponseHandler handler = new HTTP.ResponseHandler(builder.build());
+		) {
+			return results = gson.fromJson(handler.getResponse(), Results.class);
+		}
+	}
+}
---
+++ src/main/java/json/cat/facts/Results.java
@@ -0,0 +1,43 @@
+package json.cat.facts;
+
+public class Results {
+	public Result[] all;
+	
+	public Results(Result[] all) {
+		this.all = all;
+	}
+	
+	public static final class Result {
+		public String _id, text, type;
+		public User user;
+		public Integer upvotes, userUpvoted;
+		
+		public Result(String _id, String text, String type, User user, Integer upvotes, Integer userUpvoted) {
+			this._id = _id;
+			this.text = text;
+			this.type = type;
+			this.user = user;
+			this.upvotes = upvotes;
+			this.userUpvoted = userUpvoted;
+		}
+	}
+
+	public static final class User {
+		public String _id;
+		public Name name;
+		
+		public User(String _id, Name name) {
+			this._id = _id;
+			this.name = name;
+		}
+	}
+	
+	public static class Name {
+		String first, last;
+		
+		public Name(String first, String last) {
+			this.first = first;
+			this.last = last;
+		}
+	}
+}
