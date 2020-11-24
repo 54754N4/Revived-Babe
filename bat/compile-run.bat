67dc7e77a36e9483b7691031bbd6621cfbd85497
Mazen Anastas <lorenzo.mallerco@gmail.com>
2020-11-24 22:08:21 +0700

    Fix: Made compiled jar be copied to working dir to always have a backup in case compilation fails

--- bat/compile-run.bat
+++ bat/compile-run.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Compiling...
-cd ""C:\Users\mazen\Desktop\Workspaces\Java\Eclipse Workspace 2020\Revived Babe\"
+cd "C:\Users\mazen\Desktop\Workspaces\Java\Eclipse Workspace 2020\Revived Babe\"
 cmd /C .\gradlew shadowJar > shadowjar-build.log
 find "FAILED" shadowjar-build.log >nul && pause || .\bat\start-bot.bat"
--- bat/start-bot.bat
+++ bat/start-bot.bat
@@ -2,6 +2,7 @@
 echo Launching Babe ...
 cd "C:\Users\mazen\Desktop\Workspaces\Java\Eclipse Workspace 2020\Revived Babe"
 type shadowjar-build.log
-java -jar ".\build\libs\Revived Babe-2.0-all.jar"
+copy ".\build\libs\Revived Babe-2.0-all.jar" .
+java -jar ".\Revived Babe-2.0-all.jar"
 if "%ERRORLEVEL%" NEQ "0" .\bat\compile-run.bat
 exit
