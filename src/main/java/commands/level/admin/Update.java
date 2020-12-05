dee80156507f5aecdec6a26aea14095caf8c08f2
Mazen Anastas <lorenzo.mallerco@gmail.com>
2020-12-05 22:47:12 +0700

    Add: Command to check for dependencies updates

--- src/main/java/commands/level/admin/Update.java
+++ src/main/java/commands/level/admin/Update.java
@@ -1,9 +1,20 @@
 package commands.level.admin;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+
 import bot.model.UserBot;
 import commands.hierarchy.DiscordCommand;
 import commands.model.Invoker;
 import commands.name.Command;
+import lib.scrape.Github;
+import lib.scrape.Github.Dependency;
 import net.dv8tion.jda.api.entities.Message;
 
 public class Update extends DiscordCommand {
@@ -15,16 +26,40 @@ public class Update extends DiscordCommand {
 	public String helpMessage() {
 		return helpBuilder("", 
 			"-cmds or --commands\t Makes me re-discover newly created commands (without restart)",
+			"-g or --gradle\tChecks gradle for updates",
 			"Used for updating specific things. No shit lol.");
 	}
 
 	@Override
 	protected void execute(String input) throws Exception {
-		String message = "Please give me a parameter.";
 		if (hasArgs("-cmds", "--commands")) {
 			Invoker.Reflector.update();
-			message = "Successfully updated commands list.";
+			println("Successfully updated commands list.");
+		} else if (hasArgs("-g", "--gradle"))
+			handleGradle();
+		else 
+			println("Please give me a parameter.");
+	}
+
+	private void handleGradle() throws IOException {
+		String gradle = Files.readString(Paths.get("build.gradle"));
+		Map<Dependency, String> versions = new ConcurrentHashMap<>();
+		List<Dependency> updateable = new ArrayList<>();
+		for (Dependency dependency : Github.Dependency.values()) {
+			versions.put(dependency, dependency.latest.fetch());
+			logger.info("Got version {} for {}", versions.get(dependency), dependency.name());
 		}
-		println(message);
+		for (Entry<Dependency, String> entry : versions.entrySet())
+			if (!gradle.contains(entry.getValue()))
+				updateable.add(entry.getKey());
+		if (updateable.size() == 0) {
+			printlnIndependently("All up to date.");
+			return;
+		}
+		final StringBuilder sb = new StringBuilder("You have to update : ");
+		updateable.stream()
+			.map(Dependency::name)
+			.forEach(d -> sb.append(d+", "));
+		printlnIndependently(sb.delete(sb.length()-2, sb.length()).toString());
 	}
 }
---
+++ src/main/java/lib/scrape/Github.java
@@ -0,0 +1,89 @@
+package lib.scrape;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+public abstract class Github {
+	public static final Browser browser = new Browser();
+	
+	public static enum Dependency {
+		LAVAPLAYER(Github::latestLavaplayer), 
+		JDA(Github::latestJDA), 
+		NAS(Github::latestNAS),
+		REFLECTIONS(Github::latestReflections), 
+		GSON(Github::latestGson);
+		
+		public final VersionFetcher latest;
+		
+		private Dependency(VersionFetcher latest) {
+			this.latest = latest;
+		}
+		
+		@FunctionalInterface
+		public static interface VersionFetcher {
+			String fetch();
+		}
+	}
+	
+	public static String latestLavaplayer() {
+		return browser.visit("https://github.com/sedmelluq/lavaplayer/tags")
+			.waitFor(By.cssSelector(".Box-row:nth-child(2) > .flex-auto > .commit > .d-flex > .flex-auto > a"))
+			.getText()
+			.trim();
+	}
+	
+	public static String latestJDA() {
+		return browser.visit("https://ci.dv8tion.net/job/JDA/lastSuccessfulBuild")
+				.waitFor(By.cssSelector(".fileList > tbody > tr:nth-child(6) > td:nth-child(2) > a"))
+				.getText()
+				.trim()
+				.replace("JDA-", "")
+				.replace(".jar", "");
+	}
+	
+	public static String latestNAS() {
+		return browser.visit("https://api.bintray.com/packages/sedmelluq/com.sedmelluq/jda-nas")
+				.waitFor(By.cssSelector("tbody > tr.treeRow.stringRow.opened:nth-child(23) > .treeValueCell.stringCell > span > .objectBox.objectBox-string"))
+				.getText()
+				.replace('"', ' ')
+				.trim();
+	}
+	
+	public static String latestReflections() {
+		return browser.visit("https://github.com/ronmamo/reflections")
+				.waitFor(By.cssSelector("article.markdown-body.entry-content.container-lg > .highlight.highlight-text-xml > pre"))
+				.getText()
+				.replaceAll("[\\s]+", "")
+				.replaceAll(".*<version>(.*)</version>.*", "$1");
+	}
+	
+	public static String latestGson() {
+		return browser.visit("https://github.com/google/gson")
+				.waitFor(By.cssSelector("article.markdown-body.entry-content.container-lg > .highlight.highlight-source-groovy-gradle > pre > span:nth-child(2)"))
+				.getText()
+				.replace('\'', ' ')
+				.trim()
+				.split(":")[2];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		String gradle = Files.readString(Paths.get("build.gradle"));
+		List<Dependency> updateable = new ArrayList<>();
+		String version;
+		for (Dependency dependency : Github.Dependency.values()) {
+			version = dependency.latest.fetch();
+			System.out.printf("Checking %s with version %s.. ", dependency.name(), version);
+			if (!gradle.contains(version)) {
+				updateable.add(dependency);
+				System.out.println("Need to update : "+dependency);
+			} else
+				System.out.println("Up to date.");
+		}
+		browser.close();
+	}
+}
