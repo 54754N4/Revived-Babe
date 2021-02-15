5dff33938e2f5c01db5d72c2769fae84620b7f27
Mazen Anastas <lorenzo.mallerco@gmail.com>
2021-02-16 03:40:24 +0700

    Add: Blocking + non-blocking delay method decorators

---
+++ src/main/java/commands/model/ThreadSleep.java
@@ -0,0 +1,54 @@
+package commands.model;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+
+import commands.hierarchy.Command;
+
+public class ThreadSleep {
+	public static final long MAX_CHECK_RATE = 60000, MIN_CHECK_RATE = 200; // only between [200ms, 1min]
+	
+	public static long checkRateOf(long duration) {
+		long checkRate = duration/10%MAX_CHECK_RATE;	// default duration = 10*checkRate
+		if (checkRate < MIN_CHECK_RATE)
+			checkRate = MIN_CHECK_RATE;
+		return checkRate;
+	}
+	
+	public static Callable<Void> nonBlocking(final long duration, Command command) {
+		return nonBlocking(duration, command::isFinished);
+	}
+	
+	public static Callable<Void> nonBlocking(final long duration, final Supplier<Boolean> abortCondition) {
+		final long checkRate = checkRateOf(duration),
+				start = System.currentTimeMillis();
+		return () -> {
+			while (!abortCondition.get() && System.currentTimeMillis() - start < duration) 
+				Thread.sleep(checkRate);
+			return null;	// since return type is Void
+		};
+	}
+	
+	public static Callable<Void> blocking(final long duration) {
+		return () -> {
+			Thread.sleep(duration);
+			return null;
+		};
+	}
+	
+	public static Callable<Void> blocking(final Supplier<Boolean> abortCondition) {
+		return () -> {
+			while (!abortCondition.get())
+				Thread.sleep(MIN_CHECK_RATE);
+			return null;
+		};
+	}
+	
+	// Convenience methods
+	
+	public static <T> Callable<Void> waitFor(final List<Future<T>> futures) {
+		return ThreadSleep.blocking(() -> futures.stream().allMatch(Future::isDone));
+	}
+}
