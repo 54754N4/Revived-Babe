00308058cef5a915470ded93e0a44b15439cff8f
54754N4 <zero.satsana@gmail.com>
2022-03-31 03:18:37 +0300

    Fix: Made FSM graphs drawn better

--- src/main/java/lib/FSMVisualizer.java
+++ src/main/java/lib/FSMVisualizer.java
@@ -10,8 +10,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import javax.imageio.ImageIO;
 
@@ -20,8 +21,7 @@ import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
 
-import com.mxgraph.layout.mxCircleLayout;
-import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.util.mxCellRenderer;
 
 import commands.hierarchy.fsm.FSMCommand;
@@ -34,28 +34,37 @@ public class FSMVisualizer {
 	
 	public static Map<State, List<State>> extractAdjacencyList(FSMCommand command) {
 		Map<State, List<State>> map = new HashMap<>();
+		Set<State> visited = extractAllStates(command);
+		for (State state : visited) {
+			map.putIfAbsent(state, new ArrayList<>());
+			for (Transition edge : state.getTransitions())
+				map.get(state).add(edge.getNextState());
+		}
+		return map;
+	}
+	
+	public static Set<State> extractAllStates(FSMCommand command) {
 		Set<State> visited = new HashSet<>();
-		Stack<State> visit = new Stack<>();
-		State current = command.getStart();
+		Queue<State> queue = new LinkedBlockingDeque<>();
+		State current = command.getStart(), next;
 		do {
-			map.putIfAbsent(current, new ArrayList<>());
 			for (Transition edge : current.getTransitions()) {
-				if (visited.contains(edge.getNextState()))
-					continue;
-				map.get(current).add(edge.getNextState()); 
-				visit.push(edge.getNextState());
+				next = edge.getNextState();
+				if (!visited.contains(next)) {
+					queue.add(next);
+					visited.add(next);
+				}
 			}
 			visited.add(current);
-			try { current = visit.pop(); }
-			catch (Exception e) { current = null; }
-		} while (current != null);
-		return map;
+			current = queue.poll();
+		} while (!queue.isEmpty());
+		return visited;
 	}
 	
 	public static File visualise(FSMCommand command, int scaling) throws IOException {
 		Map<State, List<State>> map = extractAdjacencyList(command);
 		// Create graph
-		Graph<State, DefaultWeightedEdge> graph = GraphTypeBuilder.<State, DefaultWeightedEdge>undirected()
+		Graph<State, DefaultWeightedEdge> graph = GraphTypeBuilder.<State, DefaultWeightedEdge>directed()
 				.allowingMultipleEdges(true)
 			    .allowingSelfLoops(true)
 			    .edgeClass(DefaultWeightedEdge.class)
@@ -72,7 +81,11 @@ public class FSMVisualizer {
 	
 	public static <V, E> File drawGraph(Graph<V, E> graph, int scaling) throws IOException {
 		JGraphXAdapter<V, E> graphAdapter = new JGraphXAdapter<V, E>(graph);
-	    mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
+		mxHierarchicalLayout layout = new mxHierarchicalLayout(graphAdapter);	// mxIGraphLayout 
+	    layout.setIntraCellSpacing(200);
+	    layout.setInterHierarchySpacing(100);
+	    layout.setInterRankCellSpacing(100);
+	    layout.setParallelEdgeSpacing(10000);
 	    layout.execute(graphAdapter.getDefaultParent());
 	    BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, scaling, new Color(0,0,0,0), true, null);
 	    File imgFile = new File(GRAPH_FILE_PATH);
@@ -81,6 +94,10 @@ public class FSMVisualizer {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		visualise(new TicTacToe(null, null), 2);
+		FSMCommand fsm = new TicTacToe(null, null);
+		// {2ND=[], START=[2ND, START, END, 2ND], END=[]}
+//		System.out.println(extractAllStates(fsm));
+//		System.out.println(extractAdjacencyList(fsm));
+		visualise(fsm, 2);
 	}
 }
